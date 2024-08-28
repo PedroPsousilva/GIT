29b77b743be0f5940919b76d4a62b01bf9a1206d
PedroPsousilva <pedro.soupsilva@gmail.com>
2024-08-28 14:10:39 -0300

    API

--- src/index.js
+++ src/index.js
@@ -1,29 +1,59 @@
-const express = require('express')//Importa o módulo  Express
+const express = require("express"); //Importa o módulo  Express
 
 //Define uma classe para organizar a lógica da aplicação
-class AppController{
+class AppController {
+  constructor() {
+    //Cria uma nova instancia do Express dentro da classe
+    this.express = express();
+    //Chama o método middlewares
+    this.middlewares();
+    //Chama o método routes para definir as rotas da API
+    this.routes();
+  }
+  middlewares() {
+    //Pertimitir que a aplicação receba dados em formato JSON nas requisições
+    this.express.use(express.json());
+  }
+  //Define as rotas da nossa API
+  routes() {
+    const users = [];
 
-    constructor(){
-        //Cria uma nova instancia do Express dentro da classe
-        this.express = express();
-        //Chama o método middlewares
-        this.middlewares();
-        //Chama o método routes para definir as rotas da API
-        this.routes();
-    }
-    middlewares(){
-        //Pertimitir que a aplicação receba dados em formato JSON nas requisições
-        this.express.use(express.json());
+    this.express.post("/users", (req, res) => {
+      const { id, nome, email, senha } = req.body;
+      users.push({ id, nome, email, senha });
+      res.status(200).send({ message: "Usario cadastrado com sucesso" });
+    });
 
-    }
-    //Define as rotas da nossa API
-    routes(){
-        //Define uma rota GET para o caminho health
-        this.express.get('/health/', (req,res)=>{res.send({nome:"Pedro Paulo de Sousa Silva",CPF:"44549715871",Idade:"17" });
-        })//Essa rota é usada para verificar se API esta OK
-    }
+    this.express.post("/auth", (req, res) => {
+        const { senha, email } = req.body;
+        const user = users.find((user) => user.senha == senha && user.email == email);
+        if (user) {
+            res.status(200).send({ message: "Usario logado com sucesso" });
+        }
+        else{
+            res.status(400).send({ message: "Usario não encontrado" });
+        } 
+        
 
+      });
+    
+    this.express.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      const user = users.find((user) => user.id == id);
+
+      if (user) {
+        res.status(200).send(user);
+      } else {
+        res.status(400).send({ message: "Usario não encontrado" });
+      }
+    });
+
+    //Define uma rota GET para o caminho health
+    this.express.get("/health/", (req, res) => {
+      res.send({ TIME: "CORINTHIANS", CPF: "44549715871", Idade: "17" });
+    }); //Essa rota é usada para verificar se API esta OK
+  }
 }
 
-//Exportando a instancia de Express configurada, para que seja acessada em outros arquivos 
+//Exportando a instancia de Express configurada, para que seja acessada em outros arquivos
 module.exports = new AppController().express;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
 //Importar a instancia do Express configurada em index.js
 const app = require("./index");
-//Inicia o servidor na porta 5000, neste caso a API  sera acessivel em http://localhost:3000/
-app.listen(3000);   
+//Inicia o servidor na porta 5000, neste caso a API  sera acessivel em http://localhost:5000/
+app.listen(5000);   
